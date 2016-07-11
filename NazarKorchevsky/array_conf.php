044e037f566cf4aaf03e4f8009bf29a136d5ce59
nspace1 <nazar.korchevsky@gmail.com>
2016-07-12 02:06:41 +0300

    Update array_conf.php

--- NazarKorchevsky/array_conf.php
+++ NazarKorchevsky/array_conf.php
@@ -3,13 +3,12 @@
    define('RAND_MAX', '100');
    define('OUT_FILE', 'tmp/array.html');
    define('SORT_DEFAULT', 'ASC');
-  
-
-    $default_array = array(
-        array (1, 2, 3, 4, 5),
-        array (6, 7, 8, 9, 10),
-        array (11, 12, 13, 14, 151),
-        array (16, 17, 18, 19, 201),
-        array  (21, 22, 23, 24, 25)
-    );
+   
+   $default_array = array(
+      array (1, 2, 3, 4, 5),
+      array (6, 7, 8, 9, 10),
+      array (11, 12, 13, 14, 151),
+      array (16, 17, 18, 19, 201),
+      array  (21, 22, 23, 24, 25)
+   );
 
