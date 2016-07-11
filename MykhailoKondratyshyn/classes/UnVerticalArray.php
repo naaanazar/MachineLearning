c916d569ae82eeafce96f9f1bb379441f303f4f3
Dregan12 <kondratyshyn_mo@fizmat.tnpu.edu.ua>
2016-07-12 00:01:02 +0300

    StuctureProject

---
+++ MykhailoKondratyshyn/classes/UnVerticalArray.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dregan
+ * Date: 11.07.16
+ * Time: 23:44
+ */
+class UnVerticalArray extends  CreateArray
+{
+    public function unVerticalArray()
+    {
+        
+    }
+}
