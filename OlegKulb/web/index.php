32638f3d0dbb6ec48193c189aaf4f84ec046c2cc
exbers <exberser@gmail.com>
2016-07-12 15:24:31 +0300

    Sort arrays v-2.1

--- OlegKulb/web/index.php
+++ OlegKulb/web/index.php
@@ -2,26 +2,15 @@
 require '__DIR___' . '/../../vendor/autoload.php';
 
 use ex\app\ArraySort;
-use ex\app\ArrayView;
-use ex\app\GenerationArray;
 use ex\app\WriteFile;
 
-$generationArray = new GenerationArray();
-$arrayView = new ArrayView();
-$arraySort = new ArraySort();
-$writeFile = new WriteFile();
-
 $arraySize = 5;
-$arrayOriginal = $generationArray->makeArray($arraySize);
+$arraySort = new ArraySort($arraySize);
+//$writeFile = new WriteFile();
 
-$Array1 = $arraySort->first($arrayOriginal, $arraySize);
-$Array2 = $arraySort->second($arrayOriginal, $arraySize);
-$Array3 = $arraySort->therd($arrayOriginal, $arraySize);
-$Array4 = $arraySort->fourth($arrayOriginal, $arraySize);
+$Array1 = $arraySort->first();
+$Array2 = $arraySort->second();
+$Array3 = $arraySort->therd();
+$Array4 = $arraySort->fourth();
 
-$arrayView->viewSortArray($Array1, $arraySize);
-$arrayView->viewSortArray($Array2, $arraySize);
-$arrayView->viewSortArray($Array3, $arraySize);
-$arrayView->viewSortArray($Array4, $arraySize);
-
-$writeFile->writeArrayInFile('file.txt', $Array1);
+//$writeFile->writeArrayInFile('file.txt', $Array1);
