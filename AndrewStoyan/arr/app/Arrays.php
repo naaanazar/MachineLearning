9e95d6caf6634b290010be66c84590481556ea09
Andrew Stoyan <stoyan.andrew@gmail.com>
2016-07-12 14:15:46 +0300

    Finnaly!

--- AndrewStoyan/arr/app/Arrays.php
+++ AndrewStoyan/arr/app/Arrays.php
@@ -1,46 +1,101 @@
 <?php
-namespace app;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
-* Initialize arrays and etc.
-*
-*/
+ * Description of Arrays
+ *
+ * @author dron
+ */
 class Arrays 
 {
-	public $array;
-	public $counter = 1;
+    public $array;
+    
 
-	public function ArrayInit($number)
-	{
-		$this->$array = array();
+    public function ArrayInit($number)
+    {
+        $this->array = [];
 
         for ($i = 0; $i < $number; $i++) { 
-            $this->$array[$i] = array();
+            $this->array[$i] = [];
         }
-	}
+    }
 
-	public function ArrayFeel($number)
-	{	
-		$this->ArrayInit($number);
+    public function ArrayFeel($number)
+    {	
+        $this->ArrayInit($number);
+        $counter = 1;
 
-		for ($i = 0; $i < $number; $i++) { 
+        for ($i = 0; $i < $number; $i++) { 
             for ($j = 0; $j < $number; $j++) { 
-                $this->array[$i][$j] = $this->count;
-                $this->count++;
+                $this->array[$i][$j] = $counter++;
             }
         }
-
+        
         return $this->array;
-	}
+    }
+    
+    public function Vert($number)
+    {
+        $this->ArrayInit($number);
+        $counter = 1;
+        
+        for ($j = 0; $j < $number; $j++) { 
+            for ($i = 0; $i < $number; $i++) {  
+                $this->array[$i][$j] = $counter++;
+            }
+        }
+        
+        return $this->array;
+    }
 
-	public function SpiralFeel($number, $method)
-	{	
-		$this->ArrayInit($number);
+    public function VertRev($number)
+    {   
+        $this->ArrayInit($number);
+        $count = 1;
+        
+        for ($j = $number - 1; $j >= 0; $j--) { 
+            for ($i = $number - 1; $i >= 0; $i--) { 
+                $this->array[$i][$j] = $count++;   
+            }
+        }
+        
+        return $this->array;
+    }
+    
+    public function Snake($number)
+    {
+        $this->ArrayInit($number);
+        $count = 1;
+        
+        for ($i = 0; $i < $number; $i++) { 
+            if ($i % 2 == 0){
+                for ($j = 0; $j < $number; $j++) {
+                    $this->array[$i][$j] = $count++;
+                }
+            } else { 
+                for ($j = $number - 1; $j >= 0; $j--) {  
+                    $this->array[$i][$j] = $count++;
+                }                           
+            }
+        }
+        
+        return $this->array;
+    }
+    
+    public function SpiralFeel($number, $method)
+    {	
+        $this->ArrayInit($number);
 
-		if ($method == 1) {
-			$count1 = 1;
-		} else {
-			$count1 = $number * $number;
-		}
+        if ($method == 1) {
+            $count1 = 1;
+        } else {
+            $count1 = $number * $number;
+        }
 
         $i = $j = 0;
         $right = $number - 1;
@@ -48,9 +103,9 @@ class Arrays
 
         for ($n = 0; $n < $number * $number; ++$n) {
         	if ($method == 1) {
-            	$this->$array[$i][$j] = $count1++;
+                $this->array[$i][$j] = $count1++;
             } else {
-            	$this->$array[$i][$j] = $count1--;
+                $this->array[$i][$j] = $count1--;
             }
 
             if (($i == ($left + 1)) && ($j == $left)) { 
@@ -79,35 +134,39 @@ class Arrays
             } 
         }
 
-		return $this->array;
-	}
+        return $this->array;
+    }
 
-	public function DiagFeel($number)
-	{	
-		$this->ArrayInit($number);
-		$x = 0; 
+    public function DiagFeel($number)
+    {	
+        $this->ArrayInit($number);
+
+        $x = 0; 
         $y = 0; 
         $count2 = 1;
-        
+                    
         for ($i = 0; $i < $number; $i++) { 
             $x = 0;
-            $y = $i;         
+            $y = $i;
+            
             while ($y >= 0) {
-               	$this->$array[$x][$y] = $count2++;
+               	$this->array[$x][$y] = $count2++;
                 $x++;
                 $y--;
             }
-        }       
+        }
+                    
         for ($i = 1; $i < $number; $i++) { 
             $x = $i;
             $y = $number - 1;
                        
             while ($x <= $number - 1) {
-                $this->$array[$x][$y] = $count2++;
+                $this->array[$x][$y] = $count2++;
                 $x++;
                 $y--;
             }
         }
+
         return $this->array;
     }
 }
--- AndrewStoyan/arr/app/output.php
+++ AndrewStoyan/arr/app/output.php
@@ -1,69 +1,33 @@
 <?php
-	namespace app;
-	/**
-	* Take care about outputing things 
-	*
-	*/
-	class OutPut 
-	{
+
+    /**
+    * Take care about outputing things 
+    *
+    */
+class OutPut 
+{
 		
-		public static function Vidstup($value)
-		{
-			echo str_repeat('<br>', $value);
-		}
+    public static function Vidstup($value)
+    {
+        echo str_repeat('<br>', $value);
+    }
 
-		public static function OutPuts($value)
-		{
-			if($value < 10) {
-                echo $value.str_repeat('&nbsp;', 3);
-            } else {
-                echo $value.str_repeat('&nbsp;', 1);
-            }
-  		}
+    public static function OutPuts($value)
+    {
+        if($value < 10) {
+            echo $value.str_repeat('&nbsp;', 3);
+        } else {
+            echo $value.str_repeat('&nbsp;', 1);
+        }
+    }
 
-		public static function OutPutArray($array, $number)
-		{
-			for ($i = 0; $i < $number; $i++) { 
-                $this->Vidstup(1);
-                for ($j = 0; $j < $number; $j++) { 
-                    $this->OutPuts($array[$i][$j]);
-                }
+    public static function OutPutArray($array, $number)
+    {
+        for ($i = 0; $i < $number; $i++) { 
+            self::Vidstup(1);
+            for ($j = 0; $j < $number; $j++) { 
+                self::OutPuts($array[$i][$j]);
             }
-		}
-
-		public static function Vert($array, $number)
-		{
-			for ($j = 0; $j < $number; $j++) { 
-                $this->Vidstup(1);
-                for ($i = 0; $i < $number; $i++) {  
-                    $this->OutPuts($array[$i][$j]);
-                }
-            }
-		}
-
-		public static function VertRev($array, $number)
-		{
-			for ($j = $number - 1; $j >= 0; $j--) { 
-                $this->Vidstup(1);
-                for ($i = $number - 1; $i >= 0; $i--) { 
-                    $this->OutPuts($array[$i][$j]);   
-                }
-            }
-		}
-
-		public static function Snake($array, $number)
-		{
-			for ($i = 0; $i < $number; $i++) { 
-                $this->Vidstup(1);
-                if ($i % 2 == 0){
-                    for ($j = 0; $j < $number; $j++) {
-                        $this->OutPuts($array[$i][$j]);
-                    }
-                } else { 
-                    for ($j = $number - 1; $j >= 0; $j--) {  
-                        $this->OutPuts($array[$i][$j]);
-                    }                           
-                }
-            }
-		}
-	}
+        }
+    }
+}
