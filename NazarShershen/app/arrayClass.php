79fbd90d9aa869ce8ee3675312bcd16157f9e0af
nazar <nazar.shershen@gmail.com>
2016-07-12 23:58:05 +0300

    Snake array

--- NazarShershen/app/arrayClass.php
+++ NazarShershen/app/arrayClass.php
@@ -29,10 +29,10 @@ class arrayClass
         }        
     }
     
-    public function printArrayStraight()
+    public function printArray($array)
     {
         $array_to_print = "";
-        foreach ($this->array as $row) {
+        foreach ($array as $row) {
             foreach ($row as $item) {
                 $array_to_print .=  $item. "&nbsp;";
                 if ($item < 10) {
@@ -42,6 +42,21 @@ class arrayClass
             $array_to_print .= "</br>";
         }
         echo $array_to_print;
+    }
+    
+    public function convertToOneDimension($array)
+    {        
+        $oneDimArray = array();
+        foreach ($array as $row) {
+            foreach ($row as $item) {
+                array_push($oneDimArray, $item);
+            }
+        }
+        
+        array_unshift($oneDimArray, "");
+        unset($oneDimArray[0]);
+        
+        return $oneDimArray;
     }
     
     public function transposeArray()
@@ -60,6 +75,46 @@ class arrayClass
             $array_to_print .= "</br>";
         }
         echo $array_to_print;
+    }   
+    
+    public function snakeArray()
+    {
+        $arr = $this->convertToOneDimension($this->array);
+        $flag = true;
+        $cnt = 0;
+        $matrixSize = $this->elementsQuantity * $this->elementsQuantity;
+        for ($i = 1; $i < $matrixSize+1; $i++) {
+            
+            if ($i % 5 == 0) {                
+                echo $arr[$i] . "&nbsp;";
+                if ($cnt != 0) {
+                    for ($j = 1; $j <= $cnt; $j++) {
+                        echo $arr[++$i] . "&nbsp;";
+                        if ($j == $cnt) {
+                            echo "</br>";
+                        }
+                    }                    
+                } else {
+                    echo '<br>';
+                }  
+                $cnt++;
+                
+                if ($flag == true) {                    
+                    echo $arr[++$i] . "</br>";
+                    $flag = false;
+                } else {
+                    echo str_repeat("&nbsp;", 20);
+                    echo $arr[++$i] . "</br>";                
+                    $flag = true;
+                }
+            } else {                                
+                echo $arr[$i] . "&nbsp;";
+                if ($arr[$i] < 10) {
+                    echo "&nbsp;&nbsp;";
+                }
+            }  
+            
+        }        
     }
     
     public function transposeArrayInversion()
@@ -78,5 +133,29 @@ class arrayClass
             $array_to_print .= "</br>";
         }
         echo $array_to_print;
+    }
+    
+    public function spiralArray()
+    {
+        $array_to_print = "";
+        $spiralArray = array();
+        for ($i = 0; $i < $this->elementsQuantity; $i++)
+        {    
+            for ($j = 0; $j < $this->elementsQuantity; $j++)
+            {
+                if ($i == 0) {
+                    $array_to_print .= $this->array[$i][$j]. "&nbsp;";
+                    $spiralArray[$i][$j] = $this->array[$i][$j];
+                } elseif ($i == 1) {
+                    $spiralArray[$i][$this->elementsQuantity--] = $this->array[$i][$j];
+                } else {
+                    break;
+                }                
+            }
+            $array_to_print .= "</br>";
+        }  
+        echo "<pre>";
+        print_r($spiralArray);
+        echo "</pre>";        
     }
 }
