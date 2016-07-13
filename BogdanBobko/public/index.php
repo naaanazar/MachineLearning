a659c24d7b1172358dcffb5ccbe3c9b6301b4dd8
Bogdan Bobko <bogdan@crowdin.com>
2016-07-13 18:06:37 +0300

    - Added public directory.

--- BogdanBobko/public/index.php
+++ BogdanBobko/public/index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use CSR\App\ArraySorterFactory;
 use CSR\App\Generators\StandartArrayGenerator;
