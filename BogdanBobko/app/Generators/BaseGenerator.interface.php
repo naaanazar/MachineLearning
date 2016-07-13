c158f09b9f3e39b673ad294ffb6802daf2c00e7e
Bogdan Bobko <bogdan@crowdin.com>
2016-07-13 14:36:07 +0300

    - Moved to Composer autoloader.  - Added tests directory and dependencies.  - Some renamings to match namespaces.  - Basic logic is almost ready.

--- BogdanBobko/app/Generators/BaseGenerator.interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Generators;
-
-interface BaseArrayGenerator {
-	public static function generate();
-}
--- BogdanBobko/index.php
+++ BogdanBobko/index.php
@@ -2,14 +2,15 @@
 
 ini_set('display_errors', 1);
 
-require_once 'App/ArraySorterFactory.class.php';
-require_once 'App/Generators/ArrayGenerator.class.php';
+require_once 'vendor/autoload.php';
 
-$sorterFactory = new App\ArraySorterFactory();
-$generatedArray = \App\Generators\StandartArrayGenerator::generate();
+use CSR\App\ArraySorterFactory;
+use CSR\App\Generators\StandartArrayGenerator;
 
-//foreach($sorterFactory::getAllTypes() as $type) {
-foreach(array(\App\ArraySorterFactory::TYPE_STANDART, \App\ArraySorterFactory::TYPE_VERTICAL) as $type) {
+$sorterFactory = new ArraySorterFactory();
+$generatedArray = StandartArrayGenerator::generate();
+
+foreach ($sorterFactory::getAllTypes() as $type) {
 	$sorter = $sorterFactory->getSorter($type);
 
 	$sorter->displayTitle();
