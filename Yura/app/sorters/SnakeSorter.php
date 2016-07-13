1cb4ee15be1a3d7b9e99ac788620d532d9c7844a
Yurii <fidelite62@gmail.com>
2016-07-13 21:51:40 +0300

    structure\pr

---
+++ Yura/app/sorters/SnakeSorter.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace yu\app\sorters;
+
+/**
+ * Description of SnakeSorter
+ *
+ * @author yurii
+ */
+class SnakeSorter {
+    //put your code here
+}
