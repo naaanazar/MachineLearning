b5496bd8bcaa9b66faa0fc6e88bf6a446bbac667
Sergiy <turan.sergiy@gmail.com>
2016-07-17 18:40:30 +0300

    Updated index.php

--- TyranSergiy/public/index.php
+++ TyranSergiy/public/index.php
@@ -1,22 +1,36 @@
  <?php
 
- 
+ error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 require __DIR__."/../vendor/autoload.php";
-
 use app\Figure\Matrix;
+use app\Factory\SortFactory;
+use app\Sorter\BasicSorter;
 
 
-$matrix = new Matrix();
-$matrix->outHTML();
+  
+  
+if( isset($_POST['Size']) && isset($_POST['type'])){
+  
+    if($_POST['Size']<=16){
+    
+    $size = $_POST['Size'];
+    $type = $_POST['type'];
+    
+    
+ $matrix = new Matrix($size);
+ $sortObj = SortFactory::getSort($type);
+ $initialMatrix = $matrix->getArray();
+ 
+  $matrix->setArray( $sortObj->sort( $matrix->getArray() ) );
+  
+  $array = $matrix->getArray();
+  
+    }
+}
 
-/*
-$matrix->reverse();
-$matrix->outHTML();
 
-$matrix->clear();
-$matrix->secondReverse();
-$matrix->outHTML();
-*/
-$matrix->clear();
-$matrix->diagonalSort() ;
-$matrix->outHTML();
+
+include 'view/index.php';
+
+unset($matrix);
