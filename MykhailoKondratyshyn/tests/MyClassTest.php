ec37f332326bb319faf8569c176cb7711c2be0e4
dregan12 <kondratyshyn_mo@fizmat.tnpu.edu.ua>
2016-07-18 16:22:30 +0300

    com

--- MykhailoKondratyshyn/tests/MyClassTest.php
+++ MykhailoKondratyshyn/tests/MyClassTest.php
@@ -5,12 +5,7 @@ namespace dregan\tests;
 use dregan\application\MyClass;
 use PHPUnit_Framework_TestCase;
 
-/**
- * Created by PhpStorm.
- * User: space-user-1
- * Date: 15.07.16
- * Time: 14:30
- */
+
 class MyClassTest extends PHPUnit_Framework_TestCase
 {
     public function testPower()
