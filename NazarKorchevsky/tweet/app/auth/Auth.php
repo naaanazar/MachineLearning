fdcc8cb8068b24c5c3803d5e470eebae912eae6d
Nazar <naaa.nazar@gmail.com>
2016-08-01 18:07:31 +0300

    tweet

---
+++ NazarKorchevsky/tweet/app/auth/Auth.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once '../app/DB/QueriesToDB.php';
+
+class Auth
+{
+    public $error;
+    public $fail;
+
+    public function authorization($login, $password) {
+
+        $DB = new QueriesToDB;
+        $sql = "SELECT id, user FROM users WHERE user='$login' and password ='$password'";
+        $result = $DB->selectDB($sql);   
+
+            if ($result) {			                
+
+                if (mysqli_num_rows($result) == 1) {
+                    session_start();
+                    $row = mysqli_fetch_assoc($result);
+                    $_SESSION['auth'] = 'true';
+                    $_SESSION['user'] = $row['user'];
+                    $_SESSION['users_id'] = $row['id'];
+
+                    ini_set('session.gc_maxlifetime', 60 * 60 * 24);			 		
+                    $_SESSION['check'] = hash('ripemd128',$_SERVER['REMOTE_ADDR'] .  $_SERVER['HTTP_USER_AGENT']);			 		
+                    header("location: index.php");
+                    exit;
+                }
+                else {
+                    $log_sql =  "Wrong password or login" . "<br>" . mysqli_error($DB->getConn());
+                    echo "<script>alert ('Wrong password or login') </script>";
+                }
+            }			
+       
+    }
+    
+    function logout(){
+        session_start();
+        session_unset();
+        session_destroy();
+        header ('location:login.php');
+        exit;
+    }
+
+    function session_security(){
+        session_start();
+        if (!$_SESSION['auth']){
+            header ("location:login.php");
+            exit;
+        }
+        if ($_SESSION['check'] != hash('ripemd128',$_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT'])){
+            logout();
+        }
+    }
+    
+}
+
