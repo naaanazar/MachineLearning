5d8ace54d6ddd89c42acfcc64649a105c555cfe5
nazar <nazar.shershen@gmail.com>
2016-08-02 10:52:04 +0300

    twitter

--- NazarShershen/public/index.php
+++ NazarShershen/public/index.php
@@ -11,6 +11,29 @@ use arr\app\twitter\User;
 //include_once './htmlForAjax.html';
 
 $a = new User();
-$id = $a->signUp("Nazar", "example@gmail.com", "123");
-echo "<br>$id";
+//$id = $a->signUp("Nazar4", "example@gmail.com", "123");
+
+
+
+
+//$message = "fsagfasiugf;agsfsagfasu;gf";
+//$a->tweet(3, $message);
+
+//$p = $a->getPosts(3);
+//
+//echo "<pre>";
+//print_r($p);
+//echo "</pre>";
+
+//$a->follow(1, 3);
+//$a->follow(1, 10);
+//$a->follow(1, 2);
+
+//$a->unFollow(1, 3);
+
+$z = $a->getFollowers(1);
+
+echo "<pre>";
+var_dump($z);
+echo "</pre>";
 
