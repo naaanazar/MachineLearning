a63b2e06981e65447eb71a29b7fe888490a56c33
dregan12 <kondratyshyn_mo@fizmat.tnpu.edu.ua>
2016-08-14 23:42:34 +0300

    parser

--- MykhailoKondratyshyn/laravel/vendor/composer/autoload_namespaces.php
+++ MykhailoKondratyshyn/laravel/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpQuery' => array($vendorDir . '/coderockr/php-query/src'),
+    'Sunra\\PhpSimple\\HtmlDomParser' => array($vendorDir . '/sunra/php-simple-html-dom-parser/Src'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
--- MykhailoKondratyshyn/laravel/vendor/mykhailo/Parser.php
+++ MykhailoKondratyshyn/laravel/vendor/mykhailo/Parser.php
@@ -13,19 +13,36 @@ class Parser
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        //curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10');
 
 
 
         $page = curl_exec($ch);
 
+$doc = \phpQuery::newDocument($page);
+
+        $title = pq('g-i-tile-l clearfix')->find('g-i-tile g-i-tile-catalog g-i-large-tile-catalog');
 
 
 
+
+
+
+
+        \phpQuery::unloadDocuments($doc);
+
+
+
+
+
+//        foreach ($title as $html){
+//            echo($html);
+//            echo "<br>";
+//        }
 
 
         curl_close($ch);
-        return "<div>$page</div>";
+        return $title;
     }
 }
 
