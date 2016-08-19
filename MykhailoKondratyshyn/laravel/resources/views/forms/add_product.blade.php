ad7db197785005257f7c00730e2d48692ccf4d77
dregan12 <kondratyshyn_mo@fizmat.tnpu.edu.ua>
2016-08-19 16:01:53 +0300

    soft_delete

--- MykhailoKondratyshyn/laravel/resources/views/forms/add_product.blade.php
+++ MykhailoKondratyshyn/laravel/resources/views/forms/add_product.blade.php
@@ -1,6 +1,17 @@
 @extends('layout')
 
 @section('content')
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
 <form method="post" enctype="multipart/form-data" action="/products/save">
 
     <div class="form-group">
--- MykhailoKondratyshyn/laravel/resources/views/products/list.blade.php
+++ MykhailoKondratyshyn/laravel/resources/views/products/list.blade.php
@@ -3,17 +3,19 @@
 
 @section('content')
 @foreach($products as $product)
-
-    <p><img src="{{$product->img_url}}"></p>
-
-    <p><a href="/products/{{$product->id}}">{{$product->title}}</a>
-    <div class="col-md-2">
-        <p align="center"><a class="btn btn-danger" href="/products/{{$product->id}}/delete">Delete</a></p>
+    <div class="product">
+        <p><img src="{{$product->img_url}}"></p>
+        <a href="/products/{{$product->id}}">{{$product->title}}</a>
+        <div class="col-md-2">
+            <p align="center"><a class="btn btn-danger" href="/products/{{$product->id}}/delete">Delete</a></p>
+        </div>
     </div>
     <br>
     <br>
     <br>
-    </p>
 @endforeach
+<?php echo $products->links(); ?>
+
+
 
 @endsection
