a7054402e5b9894d4c88e22af670609ad985559b
Nazar <naaa.nazar@gmail.com>
2016-08-26 15:18:53 +0300

    add aws-sdk-php v3, add upload to s3 and delete

---
+++ MachineLearningApp/app/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'S3.php';
+
+$storage = new S3;
+//$upload = $storage->uploadFileToS3 ('Start.php');
+//echo '<a href='. $upload .'>'. $upload.'</a>';
+
+//$delete = $storage->deleteFileFromoS3 ('Start.php');
+//echo $delete . 'delete';
