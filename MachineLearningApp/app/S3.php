6e185a4a7576ad01bac6ac1dd13139e1182a005b
Bogdan Bobko <bogdan@crowdin.com>
2016-08-26 16:22:27 +0300

    - Some minor updates and formating improvements.

--- MachineLearningApp/app/S3.php
+++ MachineLearningApp/app/S3.php
@@ -11,26 +11,24 @@ use Aws\S3\Exception\S3Exception;
 
 class S3
 {
-
     public $bucket = 'ml-datasets-test';
-    
+
     private function connectToS3()
     {
-        $s3 = new Aws\S3\S3Client([
+        $s3 = new S3Client([
             'version'     => 'latest',
             'region'      => 'us-east-1',
             'credentials' => [
                 'key'    => 'AKIAI5RJSS2CYUZ6STHQ',
                 'secret' => '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'
-        ]
+            ]
         ]);
+
         return $s3;
     }
 
-
     public function uploadFileToS3 ($filepath)
     {
-
         $keyname = basename($filepath);
         $client = $this->connectToS3();
 
@@ -49,29 +47,23 @@ class S3
     }
     public function deleteFileFromoS3 ($filename)
     {
-
         $client = $this->connectToS3();
+
         try {
-            $result = $client->deleteObject([
+            $client->deleteObject([
                 'Bucket' => $this->bucket,
                 'Key' => $filename,
                 'RequestPayer' => 'requester'
             ]);                
-            
         } catch (S3Exception $e) {
             echo $e->getMessage() . "\n";
         }
-
     }
-
-
 }
-
 
 $target_dir = "../public/uploads/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-
 
 $s3 = new S3();
 $test = $s3->uploadFileToS3($target_file);
