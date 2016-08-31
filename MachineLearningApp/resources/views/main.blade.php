8a3a72d52b57a96482d31508e168315d4d425cc7
Bogdan Bobko <bogdan@crowdin.com>
2016-08-31 15:00:47 +0300

    - Favicon fix.

--- MachineLearningApp/resources/views/main.blade.php
+++ MachineLearningApp/resources/views/main.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="shortcut icon" href="images/favicon.ico">
+        <link rel="shortcut icon" href="{{ URL::to('images/favicon.ico') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- tde above 3 meta tags *must* come first in tde head; any otder head content must come *after* tdese tags -->
         <title>Crowdin Space Machine Learning App</title>
