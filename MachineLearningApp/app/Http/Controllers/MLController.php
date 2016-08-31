f35a4b9db02dd3247c167b8e0ad87302d9579dee
Nazar <nazar.korchevsky@gmail.com>
2016-08-31 15:55:27 +0300

    add

--- MachineLearningApp/app/Http/Controllers/MLController.php
+++ MachineLearningApp/app/Http/Controllers/MLController.php
@@ -7,10 +7,50 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Aws\MachineLearning\MachineLearningClient;
+use Aws\MachineLearning\Exception\MachineLearningException;
+
 class MLController extends Controller
 {
+    
+    public $bucket = 'ml-datasets-test';
+
     public function index()
     {
         return view('ml.index');
+    }
+
+    private function connectToML()
+    {
+        $ml = new MachineLearningClient([
+            'version'     => 'latest',
+            'region'      => 'us-east-1',
+            'credentials' => [
+                'key'    => 'AKIAI5RJSS2CYUZ6STHQ',
+                'secret' => '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'
+            ]
+        ]);
+
+        return $ml;
+    }
+
+
+    public function describeDataSources ()
+    {
+
+        $client = $this->connectToML();
+
+        try {
+
+            $result = $client->describeDataSources([
+                'SortOrder' => 'asc'
+            ]);
+
+        } catch (S3Exception $e) {
+            echo $e->getMessage() . "\n";
+        }
+        //echo '<pre>';
+        //print_r($result);
+        return view('ml.index',['result' => $result]);
     }
 }
--- MachineLearningApp/resources/views/ml/index.blade.php
+++ MachineLearningApp/resources/views/ml/index.blade.php
@@ -1,16 +1,8 @@
 @extends('main')
 
 @section('content')
-<div class="container">
-    <h3 class="title">ML Models</h3>
-    
-    <form class="navbar-form navbar-right" enctype="multipart/form-data" action="#" method="post">
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary">Create Model</button>
-        </div>
-    </form>
+<?php
+print_r($result);
+?>
 
-    <span>And here is emptyness...</span>
-</div>
-<br>
-@endsection
+@stop
