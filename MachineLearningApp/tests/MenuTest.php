730469e8a809f92da40e5b67267a9a03a1c4be00
yarik <oyarik@gmail.com>
2016-09-01 16:44:52 +0300

    Test click menu

---
+++ MachineLearningApp/tests/MenuTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class MenuTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->visit('/');
+    }
+
+    public function testOpenPredictionsPage(){
+        $this->click('Predictions')
+             ->seePageIs('predictions')
+             ->see('Real time prediction');
+    }
+
+    public function testopenMLPage(){
+        $this->click('ML')
+             ->seePageIs('ml')
+             ->see('ML Models');
+    }
+
+    public function testopenListS3Page(){
+        $this->click('List S3')
+             ->seePageIs('s3/list')
+             ->see('List of files');
+    }
+
+    public function testopenCeneratorPage(){
+        $this->click('Generator')
+             ->seePageIs('generator')
+             ->see('Generate dataset');
+    }
+}
