c8e5cb233928c682f39b3e888be937e0719d88e5
dregan12 <kondratyshyn_mo@fizmat.tnpu.edu.ua>
2016-09-02 10:27:57 +0300

    #9 Buckets.main

---
+++ MachineLearningApp/resources/views/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="{{ URL::to('images/favicon.ico') }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <!-- tde above 3 meta tags *must* come first in tde head; any otder head content must come *after* tdese tags -->
+        <title>Crowdin Space Machine Learning App</title>
+        <!-- Bootstrap -->
+        <link href="{{ URL::to('css/lib/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ URL::to('css/main.css') }}" rel="stylesheet">
+        <script src="{{ URL::to('js/lib/jquery/jquery.min.js') }}"></script>
+        <script src="{{ URL::to('js/common.js') }}"></script>
+        <script src="{{ URL::to('js/lib/bootstrap/bootstrap.min.js') }}"></script>
+        <script src="{{ URL::to('js/back-to-top.js') }}"></script>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view tde page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/">
+                        <img class="logo" alt="crowdin-space" src="{{ URL::to('images/crowdin-space.png') }}">
+                    </a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li {{ (Request::is('predictions') ? 'class=active' : '') }}><a href="{{ action('S3Controller@predictionForm') }}">Predictions</a></li>
+                    <li {{ (Request::is('ml') ? 'class=active' : '') }}><a href="{{ action('MLController@index') }}">ML</a></li>
+                    <li {{ (Request::is('s3/list') ? 'class=active' : '') }}><a href="{{ action('S3Controller@listS3') }}">List S3</a></li>
+                    <li {{ (Request::is('generator') ? 'class=active' : '') }}><a href="{{ URL::to('generator') }}">Generator</a></li>
+                    <li {{ (Request::is('bucket/listBucket') ? 'class=active' : '') }}><a href="{{ action('BucketController@index') }}">Buckets</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        @yield('content')
+
+        <div class="footer panel panel-margin">
+            <div class="panel-footer text-center">
+                <span class="copy">2016 &copy; Crowdin.Space</span>
+            </div>
+        </div>
+        <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Back to Top" data-toggle="tooltip" data-placement="top">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+    </body>
+</html>
