8cfb8329eb4a510df04d5e5e92c55a7dc5f1b226
Nazar Shershen <Nazar Shershen>
2016-09-05 11:57:08 +0300

    GeneratorRequest class;

---
+++ MachineLearningApp/app/Http/Requests/GeneratorRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class GeneratorRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'rows' => 'required|integer',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'rows.required' => 'Records quantity is required',
+            'rows.integer'  => 'Records quantity must be an integer',
+        ];
+    }
+}
