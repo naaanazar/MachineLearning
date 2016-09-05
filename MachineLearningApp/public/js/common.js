20b260a8a298672713a015e1cee278c7be2efe44
Bogdan Bobko <bogdan@crowdin.com>
2016-09-05 17:03:57 +0300

    - Formatting.

--- MachineLearningApp/public/js/common.js
+++ MachineLearningApp/public/js/common.js
@@ -1,41 +1,48 @@
 $(document).ready(function () {
     // tooltip from upload button
     $("[data-toggle='tooltip']").tooltip();
+
     // upload button without submit
     $('#input-file').change(function() {
         $('.form-upload').submit();
-    } ) ;
+    });
+
     //ml hide/show form create
     $('.btn-create-mlmodel').click(function(){
         $('.create-mlmodel').toggle();
         $(".container-describeMLModels").toggle();
-    });   
+    });
+
     $(".btn-create-bath-description").click(function() {
         $(".create-bath-description").toggle();
         $(".container-describeBatchPredictions").toggle();
-    } ) ;   
+    });
+
     $(".btn-create-evaluations").click(function() {
         $(".create-evaluations").toggle();
         $(".container-describeEvaluations").toggle();
-    }); 
+    });
+
     $(".btn-create-datasource").click(function() {
         $(".create-datasource").toggle();       
         $(".container-describeDataSources").toggle();      
     });
-    
-        // upload show/hide message
+
+    // upload show/hide message
     $(".upload-message").show().delay(1500).fadeOut(1000);
     // delete row from s3 table
     $.ajaxSetup({
         headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
 
-     $('.btn-delete').on('click', function(e){
+    $('.btn-delete').on('click', function(e) {
         var url = $(this).attr('href');
+
         e.preventDefault();
-        $.post(url, function( data ) {
+
+        $.post(url, function(data) {
             if(data.success) {
                 $(e.target).closest('tr').hide("slow");
             }
