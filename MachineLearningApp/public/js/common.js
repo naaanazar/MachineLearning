218032f95bc6dceca80ef876aecf5c8c884d809b
Vitaliy Olikh <vitaliy.olikh@gmail.com>
2016-09-05 10:29:31 +0300

    - Delere row from S3 list (ajax)

--- MachineLearningApp/public/js/common.js
+++ MachineLearningApp/public/js/common.js
@@ -19,12 +19,25 @@ $(document).ready(function () {
         $(".container-describeEvaluations").toggle();
     }); 
     $(".btn-create-datasource").click(function() {
-        $(".create-datasource").toggle();       
-        $(".container-describeDataSources").toggle();      
+        $(".create-datasource").toggle();
+        $(".container-describeDataSources").toggle();
     });
-     
-  
-
     // upload show/hide message
     $(".upload-message").show().delay(1500).fadeOut(1000);
+    // delete row from s3 table
+    $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+     $('.btn-delete').on('click', function(e){
+        var url = $(this).attr('href');
+        e.preventDefault();
+        $.post(url, function( data ) {
+            if(data.success) {
+                $(e.target).closest('tr').fadeOut();
+            }
+        });
+    });
 });
