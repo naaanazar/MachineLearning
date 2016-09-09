d9c20aba4f62f268186b7c72834b08ffc7e31d28
vitaliy-olikh <vitaliy.olikh@gmail.com>
2016-09-09 16:13:20 +0300

    - Add real-time prediction

--- MachineLearningApp/app/Http/Controllers/MLController.php
+++ MachineLearningApp/app/Http/Controllers/MLController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
 use App\Http\Controllers\Controller;
 use Aws\MachineLearning\MachineLearningClient;
 use Aws\MachineLearning\Exception\MachineLearningException;
@@ -227,16 +228,6 @@ class MLController extends Controller
     }
 
 
-    public function getEndpointStatus($modelId)
-    {
-        $result = $this->client->getMLModel([
-            'MLModelId' => $modelId,
-            'Verbose' => true,
-        ]);
-        return $result['EndpointInfo']['EndpointStatus'];
-    }
-
-
     public function deleteEvaluation($EvaluationId)
     {
 
@@ -417,9 +408,7 @@ class MLController extends Controller
         } catch (MachineLearningException $e) {
             echo $e->getMessage() . "\n";
         }
-
     }
-
 
     public function predict(Request $request)
     {
@@ -435,40 +424,42 @@ class MLController extends Controller
         $sameLoginAndProjectName = $request->input('same_login_and_project_name');
 
         $endPoint = $this->createRealtimeEndpoint($MLModelId);
+
+        $endpointStatus  = $endPoint["RealtimeEndpointInfo"]["EndpointStatus"];
         $predictEndpoint = $endPoint["RealtimeEndpointInfo"]["EndpointUrl"];
-        $endpointStatus = $endPoint["RealtimeEndpointInfo"]["EndpointStatus"];
 
-        // $point = 0;
-        // while ($point !== 1) {
-        //     if ($endpointStatus == "READY") $point = 1;
-        //     else sleep(2);
-        // }
+        if ($endpointStatus != 'READY') {
+            $status = 'Updating';
+            return $status;
+        } else {
+            try {
+                $result = $this->client->predict([
+                    'MLModelId'       => $MLModelId,
+                    'PredictEndpoint' => $predictEndpoint,
+                    'Record' => [
+                        "country" => $country,
+                        "members_count" => $membersCount,
+                        "strings_count" => $stringsCount,
+                        "projects_count" => $projectCount,
+                        "has_private_project" => $hasPrivateProject,
+                        "email_custom_domain" => $emailCustomDomain,
+                        "days_after_last_login" => $daysAfterLastLogin,
+                        "same_email_domain_count" => $sameEmailDomainCount,
+                        "same_login_and_project_name" => $sameLoginAndProjectName,
+                    ]
+                ]);
 
-        try {
-            $result = $this->client->predict([
-                'MLModelId' => $MLModelId, // REQUIRED
-                'PredictEndpoint' => $predictEndpoint, // REQUIRED
-                'Record' => [
-                    "email_custom_domain" => $emailCustomDomain,
-                    "same_email_domain_count" => $sameEmailDomainCount,
-                    "projects_count" => $projectCount,
-                    "strings_count" => $stringsCount,
-                    "members_count" => $membersCount,
-                    "has_private_project" => $hasPrivateProject,
-                    "same_login_and_project_name" => $sameLoginAndProjectName,
-                    "days_after_last_login" => $daysAfterLastLogin,
-                    "country" => $country
-                ]
-            ]);
+            } catch (MachineLearningException $e) {
+                echo $e->getMessage() . "\n";
+            }
 
-        } catch (MachineLearningException $e) {
-            echo $e->getMessage() . "\n";
+            $result = "<h3><strong>Prediction:</strong> " . $result["Prediction"]["predictedLabel"] . "</h3>";
+
+            $this->deleteRealtimeEndpoint($MLModelId);
+
+            if($request->ajax()) return $result;
+            else return false;
         }
-
-        $this->deleteRealtimeEndpoint($MLModelId);
-        $result = "Prediction: " . $result["Prediction"]["predictedLabel"];
-
-        return redirect('/predictions')->with('result', $result);
     }
 
     public function updateDataSource($DataSourceId)
