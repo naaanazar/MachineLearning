f4fe618827297a77e0645836267482ffd9a704d9
Nazar Shershen <Nazar Shershen>
2016-09-12 16:44:03 +0300

    Generator fix

--- MachineLearningApp/resources/views/generator/generator.blade.php
+++ MachineLearningApp/resources/views/generator/generator.blade.php
@@ -5,25 +5,18 @@
 <script type="text/javascript" src="{{ URL::to('js/generate-dataset.js') }}"></script>
 
 <div class="container">    
-    <div class="row generator-form col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div>
-            <h3 align='center'>Generate dataset</h3>
-            <div>    
-                <div class="col-lg-11 col-md-10 col-sm-10 col-xs-10" >          
-                <input style="display: inline-block; margin-top: 20px;" class="form-control" id="rows-number" ondrop="return false" name="rows" placeholder="Records quantity" min="1">
-                </div>
-                <div class="col-lg-1 col-md-2 col-sm-2 col-xs-2" >
-                    <a id="generate-btn" class="btn btn-info disabled"; style="margin-top: 20px; margin-left:-25px;"; href="{{ URL::to('generate') }}">Generate</a>
-                    <div class='empty-msg'>    
-                    </div>
-                </div>
-            </div>
-            <div style="text-align: center; padding-top: 50px;">
-                <i class="fa fa-spinner fa-4x fa-spin" style="display: none;"></i>
-            </div>
-            <div class="messages">
-            </div>
-        </div>    
+    <div class="row generator-form col-xs-12">
+        <h3 align='center'>Generate dataset</h3> 
+        <div class="col-md-10 col-xs-12 generator-input-container">
+            <input class="form-control" id="rows-number" ondrop="return false" name="rows" placeholder="Records quantity" min="1">
+            <div class='empty-msg'></div>
+        </div>
+        <div class="col-md-2 col-xs-4 generator-button-container">
+            <a id="generate-btn" class="btn btn-info disabled" href="{{ URL::to('generate') }}">Generate</a>
+        </div>
+        <div class="spinner-warapper">
+            <i class="fa fa-spinner fa-4x fa-spin" style="display: none;"></i>
+        </div> 
     </div>
 </div>
     
