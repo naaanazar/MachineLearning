8747e4fcd38c25574116e1710f49e30b3870f238
dregan12 <kondratyshyn_mo@fizmat.tnpu.edu.ua>
2016-09-14 15:55:44 +0300

    Validation dataSource

--- MachineLearningApp/public/js/ml/tabDataSource.js
+++ MachineLearningApp/public/js/ml/tabDataSource.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     listDataSource();
 
@@ -24,27 +24,24 @@ $(document).ready(function() {
         $(".container-describeDataSources").toggle();
 
         var resp = '';
-        $('input#DataSourceName').unbind().blur( function(){
+        $('input#DataSourceName').unbind().blur(function () {
             var id = $(this).attr('id');
             var val = $(this).val();
             $(this).closest('div').find('span').addClass('hide');
 
-            switch(id)
-            {
+            switch (id) {
                 case 'DataSourceName':
                     var rv_name = /^[a-zA-Zа-яА-Я]{1,40}$/;
 
-                    if(val.length > 2 && val != '' && rv_name.test(val))
-                    {
+                    if (val.length > 2 && val != '' && rv_name.test(val)) {
                         $(this).removeClass('error').addClass('not_error');
                         $(this).closest('div').removeClass('has-error');
                         $(this).closest('div').addClass('has-success has-feedback');
                         $(this).closest('div').find('span').removeClass('hide');
-resp = 'OK';
+                        resp = 'OK';
 
                     }
-                    else
-                    {
+                    else {
                         $(this).removeClass('not_error').addClass('error');
                         $(this).closest('div').addClass('has-error has-feedback');
                         $(this).closest('div').find('span').addClass('hide');
@@ -53,25 +50,29 @@ resp = 'OK';
                     break;
             }
 
+            console.log(resp);
+            if (resp !== 'OK') {
+                $(this).closest('form').find('button').addClass('disabled');
+
+            } else {
+                $(this).closest('form').find('button').removeClass('disabled');
+            }
         });
 
-console.log(resp);
-if (resp == 'OK'){
-        $.get("/ml/select-S3objects", function(response){
-            var  result;
+
+
+        $.get("/ml/select-S3objects", function (response) {
+            var result;
             for (var key in response.data) {
                 result += '<option value="' + response.data[key].Key + '">' + response.data[key].Key + '</option>';
             }
             $('#SelectDataLocationS3').html(result);
         });
-             }else {
-                $(this).closest('form').find('button').addClass('disabled');
-        }
     });
 
     $(document).on("click", '#describeDataSourcesContent', function () {
         listDataSource();
-     });
+    });
 
     //loading data(tabs)
     // $('#describeDataSourcesContent').on('click', function() {
@@ -83,44 +84,44 @@ if (resp == 'OK'){
         var button = '<button class="btn btn-primary btn-create-datasource pull-right">Create Datasource</button>'
         $('#ml-button-create').html(button);
 
-        $.get("/ml/describe-data-sources", function(response) {
+        $.get("/ml/describe-data-sources", function (response) {
 
-            var i=1;
+            var i = 1;
             var res = '' +
                 '<table class="table table-bordered table-font text-center">' +
-                                '<tr class="active">' +
-                                    '<td>Name</td>' +
-                                    '<td>Status</td>' +
-                                    '<td>DataLocationS3</td>' +
-                                    '<td>Last Updated</td>' +
-                                    '<td>&nbsp;</td>' +
-                                '</tr>' +
-                                '<span class="hide">' + i + '</span>';
-                                for (var key in response.data) {
-                                    i = i+1;
-                                    date = response.data[key].LastUpdatedAt.replace('T', '  ');
-                                    date = date.substring(0, date.indexOf('+'));
-                                    res += '' +
-                                    '<tr>' +
-                                        '<td class="hide">' + response.data[key].DataSourceId + '</td>' +
-                                        '<td>';
-                                            if (response.data[key].Name !== undefined) {
-                                                res += response.data[key].Name;
-                                            }
-                                        res += '' +
-                                        '</td>' +
-                                        '<td>' + response.data[key].Status + '</td>' +
-                                        '<td>' + response.data[key].DataLocationS3 + '</td>' +
-                                        '<td>' + date + '</td>' +
-                                        '<td>' +
-                                            '<a class="btn btn-info btn-sm btn-list datasource-info" href="#modal"' + 'data-toggle="modal" id="info_' + i + '" data-source-id="' + response.data[key].DataSourceId + '"><span ' +
-                                                        'class="glyphicon glyphicon-info-sign"></span></a>&nbsp;' +
-                                            '<a class="btn btn-danger btn-sm btn-list delete" href="#" data-delete-id="' + response.data[key].DataSourceId + '"><span class="glyphicon glyphicon-trash"></span></a>' +
-                                        '</td>' +
-                                    '</tr>' +
-                                    '<span class="hide">' + i + '</span>';
-                                }
-                            res += '</table>';
+                '<tr class="active">' +
+                '<td>Name</td>' +
+                '<td>Status</td>' +
+                '<td>DataLocationS3</td>' +
+                '<td>Last Updated</td>' +
+                '<td>&nbsp;</td>' +
+                '</tr>' +
+                '<span class="hide">' + i + '</span>';
+            for (var key in response.data) {
+                i = i + 1;
+                date = response.data[key].LastUpdatedAt.replace('T', '  ');
+                date = date.substring(0, date.indexOf('+'));
+                res += '' +
+                    '<tr>' +
+                    '<td class="hide">' + response.data[key].DataSourceId + '</td>' +
+                    '<td>';
+                if (response.data[key].Name !== undefined) {
+                    res += response.data[key].Name;
+                }
+                res += '' +
+                    '</td>' +
+                    '<td>' + response.data[key].Status + '</td>' +
+                    '<td>' + response.data[key].DataLocationS3 + '</td>' +
+                    '<td>' + date + '</td>' +
+                    '<td>' +
+                    '<a class="btn btn-info btn-sm btn-list datasource-info" href="#modal"' + 'data-toggle="modal" id="info_' + i + '" data-source-id="' + response.data[key].DataSourceId + '"><span ' +
+                    'class="glyphicon glyphicon-info-sign"></span></a>&nbsp;' +
+                    '<a class="btn btn-danger btn-sm btn-list delete" href="#" data-delete-id="' + response.data[key].DataSourceId + '"><span class="glyphicon glyphicon-trash"></span></a>' +
+                    '</td>' +
+                    '</tr>' +
+                    '<span class="hide">' + i + '</span>';
+            }
+            res += '</table>';
 
             $('.container-describeDataSources').html(res);
         });
