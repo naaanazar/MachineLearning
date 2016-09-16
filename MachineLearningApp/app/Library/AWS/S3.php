a587f9fdc7a5423e61d2ad778e54daa3912e70e3
Nazar <naaa.nazar@gmail.com>
2016-09-16 15:14:11 +0300

    add methods S3Controller use in ML to S3.php

---
+++ MachineLearningApp/app/Library/AWS/S3.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Library\AWS;
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+class S3
+{
+    private $bucket = 'ml-datasets-test';
+    private $client;
+
+       function __construct()
+    {
+        $this->client = $this->connect();
+    }
+
+    private function connect()
+    {
+        $s3 = new S3Client([
+            'version'     => 'latest',
+            'region'      => 'us-east-1',
+            'credentials' => [
+                'key'    => 'AKIAI5RJSS2CYUZ6STHQ',
+                'secret' => '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'
+            ]
+        ]);
+
+        return $s3;
+    }
+
+    public function getClient()
+    {
+        $client = $this->connect();
+        return $client;
+    }
+
+     public function ListObjectsS3()
+    {
+        $client = $this->connect();
+
+        try {
+            $result = $client->listObjects([
+                'Bucket' => $this->bucket,
+
+            ]);
+
+            $results = $result['Contents'];
+
+        } catch (S3Exception $e) {
+            echo $e->getMessage() . "\n";
+        }
+
+        return $results;
+    }
+
+
+    public function getObjectACL()
+    {
+    $client = $this->connect();
+    $result = $client->getObjectAcl([
+        'Bucket' => $this->bucket, // REQUIRED
+        'Key' => 'dataset.csv', // REQUIRED
+        'RequestPayer' => 'requester',
+
+    ]);
+
+    dd($result);
+    }
+}
