627d9970b2b059b1df934b4df55e6a4853994b52
Yurii <fidelite62@gmail.com>
2016-09-21 12:50:42 +0300

    animated F-page/blades

--- MachineLearningApp/public/js/startPage/roll-down.js
+++ MachineLearningApp/public/js/startPage/roll-down.js
@@ -3,7 +3,14 @@ $(document).ready(function () {
         var elementClick = $(this).attr("href");
         var destination = $(elementClick).offset().top;
             $('html,body').animate({ scrollTop: destination }, 1200,'swing');
-        
         return false; 
     });
 });
+
+$(document).ready(function () {
+        $("#button-benefits").click(function() {
+            $('html,body').animate({
+                scrollTop: $(".benefits-title").offset().top},
+                'slow');
+        });
+    });
