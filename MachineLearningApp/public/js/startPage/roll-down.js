df420e8f3916449b48c00dde16e6f4d8831b8fa4
Yurii <fidelite62@gmail.com>
2016-09-22 14:39:22 +0300

    roll-down-page fix

--- MachineLearningApp/public/js/startPage/roll-down.js
+++ MachineLearningApp/public/js/startPage/roll-down.js
@@ -7,9 +7,7 @@ $(document).ready(function() {
         }, 1200, 'swing');
         return false;
     });
-});
 
-$(document).ready(function() {
     $("#button-benefits").click(function() {
         $('html,body').animate({
                 scrollTop: $(".benefits-title").offset().top
