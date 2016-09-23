2b640fa53d3594714e1e6ddc38b19f1374868227
Vitaliy Olikh <vitaliy.olikh@gmail.com>
2016-09-23 16:59:12 +0300

    - S3: add validtation Bucket

--- MachineLearningApp/public/js/s3/tabS3.js
+++ MachineLearningApp/public/js/s3/tabS3.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    function validationBucket(selector, lengthVal, regexp) {
+        $('.create-datasource').on('input', selector, function (e){
+            this.valueField = $(e.target).val();
+            this.regExp = new RegExp(regexp, "g");
 
+            this.newValue = this.valueField.replace(this.regExp, "");
+            $(selector).val(this.newValue.substr(0, lengthVal));
+        });
+    }
 
+    validationBucket("#nameBucket", 255, "^ |[^0-9a-zA-Z-._]");
 });
